f908bd83fa1857e30d61e85d0fa3c3d55a351c17
biajixh <xuhao@123>
2022-08-02 15:22:04 +0800

    [00001][开发学习]仓库构建

---
+++ src/components/home/AppIndex.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Hello World,biaji!
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default{
+  name: 'AppIndex'
+}
+</script>
+
+<style scoped>
+
+</style>
