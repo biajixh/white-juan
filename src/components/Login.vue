9c113f60a3b7c614d55c2101414b3afb8ed9a8de
biajixh <xuhao@123>
2022-08-14 18:48:40 +0800

    [00003][学习开发]修改背景图

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,18 @@
 <template>
-    <el-form class="login-container" lable-position="left" label-wdith="0px">
-        <h3 class="login-title">系统登录</h3>
-        <el-form-item>
-            <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="账号"></el-input>
-        </el-form-item>
-        <el-form-item>
-            <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
-        </el-form-item>
-        <el-form-item width="100%">
-            <el-button type="primary" style="width: 100%;background-color: #505458;border: none;" v-on:click="login">登录</el-button>
-        </el-form-item>
-    </el-form>
+    <body id="poster">
+        <el-form class="login-container" lable-position="left" label-wdith="0px">
+            <h3 class="login-title">系统登录</h3>
+            <el-form-item>
+                <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="账号"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="密码"></el-input>
+            </el-form-item>
+            <el-form-item width="100%">
+                <el-button type="primary" style="width: 100%;background-color: #505458;border: none;" v-on:click="login">登录</el-button>
+            </el-form-item>
+        </el-form>
+    </body>
 </template>
 
 <script>
@@ -58,5 +60,20 @@ export default{
     margin: 0px auto 40px auto;
     text-align: center;
     color: #505458;
+}
+
+#poster {
+    background: url("../assets/background.jpg") no-repeat;
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+}
+#app {
+    margin:0px;
+}
+body {
+    margin:0px;
 }
 </style>
